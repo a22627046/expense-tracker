300aa4be8dbd4567d440a49383073443e9f6035f
Yu-Ting Wu <a22627046@gmail.com>
2022-03-06 23:41:52 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
+//設定連線到 mongoDB
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+//取得連線狀態
+const db = mongoose.connection
+//連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+//連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
 app.get('/', (req, res) => {
   res.send('hello world!')
 })
+
 app.listen(3000, () => {
-  console.log('App is runnig on http://localhost:3000')
+  console.log('App is running on http://localhost:3000')
 })
