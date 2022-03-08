b0e4b5306a4cad682ca0833166f3d61859fcca69
Yu-Ting Wu <a22627046@gmail.com>
2022-03-09 01:25:04 +0800

    feat: add navbar

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ db.once('open', () => {
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
