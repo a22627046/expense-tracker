14451df4ef94ff338897863af868354d520ff433
Yu-Ting Wu <a22627046@gmail.com>
2022-03-08 22:42:51 +0800

    feat: add category model

---
+++ models/category.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  icon: {
+    type: String,
+    require: true
+  },
+  records: {
+    type: Schema.Types.ObjectId,
+    ref: 'Record'
+  }
+})
+module.exports = mongoose.model('Category', categorySchema)
