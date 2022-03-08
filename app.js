5f5d2be7c3e5d0cac36b4c0956c99b8afba07896
Yu-Ting Wu <a22627046@gmail.com>
2022-03-09 00:12:55 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 
 const app = express()
@@ -16,9 +17,12 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  res.render('index')
 })
 
 app.listen(3000, () => {
