c42b0be91276edeccd0a3a87f4857067a980b3fa
Yu-Ting Wu <a22627046@gmail.com>
2022-03-12 00:56:49 +0800

    feat: add delete record function

--- app.js
+++ app.js
@@ -90,6 +90,14 @@ app.put('/records/:id', (req, res) => {
     .catch(error => console.error(error))
 })
 
+app.delete('/records/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+})
+
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
