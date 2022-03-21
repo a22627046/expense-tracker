0aafe2dd54bc1582005e5291c165022671506025
Yu-Ting Wu <a22627046@gmail.com>
2022-03-22 00:08:33 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const user = require('../../models/user')
 const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
@@ -9,7 +10,8 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/users/login'
+  failureRedirect: '/users/login',
+  failureMessage: true
 }))
 
 router.get('/register', (req, res) => {
@@ -19,13 +21,30 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   //取得註冊表單參數
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符。' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   //檢查使用者是否已經註冊
   User.findOne({ email })
     .then(user => {
       //如果已註冊，退回原本畫面
       if (user) {
-        console.log('User already exists.')
-        res.render('register', {
+        errors.push({ message: '這個Email已經註冊過了。' })
+        return res.render('register', {
+          errors,
           name,
           email,
           password,
@@ -46,6 +65,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
